bf67555e510a327c4981bbc1a1ba569c035ffc7a
27kan <27kan@ezweb.ne.jp>
2021-03-06 12:17:35 +0900

    記入漏れしていたインスタンス作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
     redirect_to "/tweets/#{comment.tweet.id}"
     #コメントを保存したらコメント投稿完了画面ではなく元のツイートにリダイレクトで返す
     #redirect_to の後にPrefixを指定する
